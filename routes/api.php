7239c77c7063e65143e78d53cf9e0cb1a33ef8ad
leluan <levanluan090201>
2022-10-26 23:17:32 +0700

    Edit .env, remove prefix in routes

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
+    'middleware' => 'api'
 ], function ($router) {
 
     Route::post('login', 'AuthController@login');
