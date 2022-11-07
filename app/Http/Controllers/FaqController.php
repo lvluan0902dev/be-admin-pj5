bc4822b5b0dfc0de4517aa90b4511304731c3240
Le Van Luan <lvluan0902dev@gmail.com>
2022-11-07 22:07:13 +0700

    Faq - Complete controller

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -6,6 +6,8 @@ use App\Models\Faq;
 use App\Repositories\BaseRepository;
 use App\Traits\ResponseTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class FaqController extends Controller
 {
@@ -21,6 +23,11 @@ class FaqController extends Controller
      */
     private  $baseRepository;
 
+    /**
+     * FaqController constructor.
+     * @param Faq $faq
+     * @param BaseRepository $baseRepository
+     */
     public function __construct(
         Faq $faq,
         BaseRepository $baseRepository
@@ -28,5 +35,183 @@ class FaqController extends Controller
     {
         $this->faq = $faq;
         $this->baseRepository = $baseRepository;
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function list(Request $request)
+    {
+        $query = $this->faq;
+
+        $params = $request->all();
+
+        $total = $query->count();
+
+        // Search
+        if (isset($params['search']) && !empty($params['search'])) {
+            $query = $query
+                ->where('title', 'LIKE', '%' . $params['search'] . '%')
+                ->orWhere('content', 'LIKE', '%' . $params['search'] . '%');
+        }
+
+        // Sort
+        $query = $this->baseRepository->sort($query, $params);
+
+        $total_result = $query->count();
+
+        // Paginate
+        $result = $this->baseRepository->paginate($query, $params);
+
+        return $this->responseJson([
+            'data' => $result['data']->items(),
+            'total_result' => $total_result,
+            'total' => $total,
+            'page' => $result['page'],
+            'last_page' => ceil($total_result / $result['per_page'])
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function add(Request $request)
+    {
+        $data = $request->all();
+
+        $data['status'] = $this->baseRepository->convertStatus($data['status']);
+
+        DB::beginTransaction();
+        try {
+            $this->faq
+                ->create([
+                    'title' => $data['title'],
+                    'content' => $data['content'],
+                    'status' => $data['status']
+                ]);
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e->getMessage() . '. Line: ' . $e->getLine());
+            return $this->responseJson([
+                'success' => 0,
+                'message' => $e->getMessage()
+            ]);
+        }
+
+        return $this->responseJson([
+            'success' => 1,
+            'message' => 'Thêm Câu hỏi thường gặp thành công'
+        ]);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function get($id)
+    {
+        $faq = $this->faq
+            ->find($id);
+
+        if (!$faq) {
+            return $this->responseJson([
+                'success' => 0,
+                'message' => 'Câu hỏi thường gặp không tồn tại hoặc đã bị xoá'
+            ]);
+        }
+
+        return $this->responseJson([
+            'success' => 1,
+            'data' => $faq
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function edit(Request $request)
+    {
+        $data = $request->all();
+
+        $faq = $this->faq
+            ->find($data['id']);
+
+        if (!$faq) {
+            return $this->responseJson([
+                'success' => 0,
+                'message' => 'Câu hỏi thường gặp không tồn tại hoặc đã bị xoá'
+            ]);
+        }
+
+        $data['status'] = $this->baseRepository->convertStatus($data['status']);
+
+        DB::beginTransaction();
+        try {
+            $faq
+                ->update([
+                    'title' => $data['title'],
+                    'content' => $data['content'],
+                    'status' => $data['status']
+                ]);
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e->getMessage() . '. Line: ' . $e->getLine());
+            return $this->responseJson([
+                'success' => 0,
+                'message' => $e->getMessage()
+            ]);
+        }
+
+        return $this->responseJson([
+            'success' => 1,
+            'message' => 'Sửa Câu hỏi thường gặp thành công'
+        ]);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function delete($id)
+    {
+        $faq = $this->faq
+            ->find($id);
+
+        if (!$faq) {
+            return $this->responseJson([
+                'success' => 0,
+                'message' => 'Câu hỏi thường gặp không tồn tại hoặc đã bị xoá'
+            ]);
+        }
+
+        DB::beginTransaction();
+        try {
+            if ($faq->delete()) {
+
+            } else {
+                return $this->responseJson([
+                    'success' => 0,
+                    'message' => 'Xoá Câu hỏi thường gặp không thành công'
+                ]);
+            }
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e->getMessage() . '. Line: ' . $e->getLine());
+            return $this->responseJson([
+                'success' => 0,
+                'message' => $e->getMessage()
+            ]);
+        }
+
+        return $this->responseJson([
+            'success' => 1,
+            'message' => 'Xoá Câu hỏi thường gặp thành công'
+        ]);
     }
 }
