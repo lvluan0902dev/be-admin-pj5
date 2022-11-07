6704bffbcb95d391022668e1ef1f6cbb25df8c12
leluan <levanluan090201>
2022-11-07 21:50:10 +0700

    Faq - Create controller

--- app/Http/Controllers/FaqController.php
+++ app/Http/Controllers/FaqController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Faq;
+use App\Repositories\BaseRepository;
+use App\Traits\ResponseTrait;
 use Illuminate\Http\Request;
 
 class FaqController extends Controller
 {
-    //
+    use ResponseTrait;
+
+    /**
+     * @var Faq
+     */
+    private $faq;
+
+    /**
+     * @var BaseRepository
+     */
+    private  $baseRepository;
+
+    public function __construct(
+        Faq $faq,
+        BaseRepository $baseRepository
+    )
+    {
+        $this->faq = $faq;
+        $this->baseRepository = $baseRepository;
+    }
 }
